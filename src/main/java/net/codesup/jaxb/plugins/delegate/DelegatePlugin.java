a949cf9b3d0e99e1f7e8e463d0bba7f19427f19a
Mirko Klemm <Mirko.Klemm@dvbbank.com>
2016-04-27 17:28:20 +0200

    First working version

--- src/main/java/net/codesup/jaxb/plugins/delegate/DelegatePlugin.java
+++ src/main/java/net/codesup/jaxb/plugins/delegate/DelegatePlugin.java
@@ -23,13 +23,9 @@
  */
 package net.codesup.jaxb.plugins.delegate;
 
-import java.io.IOException;
-import java.text.MessageFormat;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.ResourceBundle;
 
 import javax.xml.bind.JAXBContext;
@@ -37,6 +33,8 @@ import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
+import com.kscs.util.plugins.xjc.base.AbstractPlugin;
+import com.kscs.util.plugins.xjc.base.Opt;
 import com.sun.codemodel.JClass;
 import com.sun.codemodel.JCodeModel;
 import com.sun.codemodel.JConditional;
@@ -47,9 +45,7 @@ import com.sun.codemodel.JInvocation;
 import com.sun.codemodel.JMethod;
 import com.sun.codemodel.JMod;
 import com.sun.codemodel.JType;
-import com.sun.tools.xjc.BadCommandLineException;
 import com.sun.tools.xjc.Options;
-import com.sun.tools.xjc.Plugin;
 import com.sun.tools.xjc.model.CPluginCustomization;
 import com.sun.tools.xjc.outline.ClassOutline;
 import com.sun.tools.xjc.outline.Outline;
@@ -62,8 +58,8 @@ import org.xml.sax.SAXException;
  *
  * @author Mirko Klemm 2015-01-22
  */
-public class DelegatePlugin extends Plugin {
-	public static final JAXBContext JAXB_CONTEXT;
+public class DelegatePlugin extends AbstractPlugin {
+	private static final JAXBContext JAXB_CONTEXT;
 
 	static {
 		try {
@@ -73,36 +69,24 @@ public class DelegatePlugin extends Plugin {
 		}
 	}
 
-	public static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(DelegatePlugin.class.getName());
-	public static final String OPTION_NAME = "-Xdelegate";
-	public static final String CUSTOMIZATION_NS = "http://www.codesup.net/jaxb/plugins/delegate";
-	public static final String DELEGATES_CUSTOMIZATION_NAME = "delegates";
-	public static final String DELEGATE_CUSTOMIZATION_NAME = "delegate";
-	public static final List<String> CUSTOM_ELEMENTS = Arrays.asList(
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(DelegatePlugin.class.getName());
+	private static final String OPTION_NAME = "-Xdelegate";
+	private static final String CUSTOMIZATION_NS = "http://www.codesup.net/jaxb/plugins/delegate";
+	private static final String DELEGATES_CUSTOMIZATION_NAME = "delegates";
+	private static final String DELEGATE_CUSTOMIZATION_NAME = "delegate";
+	private static final String METHOD_CUSTOMIZATION_NAME = "method";
+	private static final List<String> CUSTOM_ELEMENTS = Arrays.asList(
 			DelegatePlugin.DELEGATES_CUSTOMIZATION_NAME,
-			DelegatePlugin.DELEGATE_CUSTOMIZATION_NAME);
-	public static final String DEFAULT_DELEGATE_FIELD_PATTERN = "__delegate%s";
-	private boolean inferMethods = true;
-	public final Map<String, Setter<String>> setters = new HashMap<String, Setter<String>>() {{
-		put("-infer-methods", new Setter<String>() {
-			public void set(final String val) {
-				DelegatePlugin.this.inferMethods = Boolean.valueOf(val);
-			}
-		});
-	}};
+			DelegatePlugin.DELEGATE_CUSTOMIZATION_NAME,
+			DelegatePlugin.METHOD_CUSTOMIZATION_NAME);
+	private static final String DEFAULT_DELEGATE_FIELD_PATTERN = "__delegate%s";
+
+	@Opt
+	private boolean inferMethods = false;
 
 	@Override
 	public String getOptionName() {
 		return DelegatePlugin.OPTION_NAME.substring(1);
-	}
-
-	@Override
-	public int parseArgument(final Options opt, final String[] args, final int i) throws BadCommandLineException, IOException {
-		int currentIndex = i;
-		if ((DelegatePlugin.OPTION_NAME).equals(args[i])) {
-			currentIndex = parseOptions(args, i, this.setters);
-		}
-		return currentIndex - i + 1;
 	}
 
 	@Override
@@ -127,6 +111,7 @@ public class DelegatePlugin extends Plugin {
 			for (final ClassOutline classOutline : outline.getClasses()) {
 				final CPluginCustomization delegatesCustomization = getCustomizationElement(classOutline, DelegatePlugin.DELEGATES_CUSTOMIZATION_NAME);
 				if (delegatesCustomization != null) {
+					delegatesCustomization.markAsAcknowledged();
 					final JAXBElement<Delegates> delegatesElement = unmarshaller.unmarshal(delegatesCustomization.element, Delegates.class);
 					for (final Delegate delegate : delegatesElement.getValue().getDelegate()) {
 						generateDelegateReference(outline, errorHandler, classOutline, delegate);
@@ -134,6 +119,7 @@ public class DelegatePlugin extends Plugin {
 				} else {
 					final CPluginCustomization delegateCustomization = getCustomizationElement(classOutline, DelegatePlugin.DELEGATE_CUSTOMIZATION_NAME);
 					if (delegateCustomization != null) {
+						delegateCustomization.markAsAcknowledged();
 						final JAXBElement<Delegate> delegateElement = unmarshaller.unmarshal(delegateCustomization.element, Delegate.class);
 						generateDelegateReference(outline, errorHandler, classOutline, delegateElement.getValue());
 					}
@@ -145,55 +131,42 @@ public class DelegatePlugin extends Plugin {
 		}
 	}
 
-	private void generateDelegateReference(final Outline outline, final ErrorHandler errorHandler, final ClassOutline classOutline, final Delegate delegate) throws SAXException {
+	private void generateDelegateReference(final Outline outline, final ErrorHandler errorHandler, final ClassOutline classOutline, final Delegate delegate) {
 		final JCodeModel model = outline.getCodeModel();
 		final JClass delegateClass = model.ref(delegate.getClazz());
 		final String delegateFieldName = String.format(DelegatePlugin.DEFAULT_DELEGATE_FIELD_PATTERN, delegateClass.name());
+		final boolean staticDelegate = coalesce(delegate.isStatic(), Boolean.FALSE);
 		final JDefinedClass definedClass = classOutline.implClass;
 		final JFieldVar delegateField = delegate.isStatic() ? null : definedClass.field(JMod.PRIVATE | JMod.TRANSIENT, delegateClass, delegateFieldName, JExpr._null());
 		for (final Method method : delegate.getMethod()) {
-			final String defaultModifiers = delegate.isStatic() ? "public static" : "public";
-			final int modifiers = parseModifiers(coalesce(method.getModifiers(), defaultModifiers));
+			final boolean staticMethod = coalesce(method.isStatic(), Boolean.FALSE);
+			final int modifiers = parseModifiers(coalesce(method.getModifiers(), "public"));
 			final JType returnType = parseType(model, method.getType());
-			final JMethod implMethod = definedClass.method(modifiers, returnType, method.getName());
+			final JMethod implMethod = definedClass.method(staticMethod ? JMod.STATIC | modifiers : modifiers, returnType, method.getName());
 			for (final MethodParameterType param : method.getParam()) {
 				final JType paramType = parseType(model, param.getType());
 				implMethod.param(paramType, param.getName());
 			}
 			final JInvocation invoke;
-			if (delegate.isStatic()) {
-				invoke = delegateClass.staticInvoke(method.getName()).arg(JExpr._this());
+			if (staticDelegate) {
+				invoke = delegateClass.staticInvoke(method.getName());
+				if(!staticMethod) {
+					invoke.arg(JExpr._this());
+				}
 			} else {
-				final JConditional ifStatement = implMethod.body()._if(delegateField.eq(JExpr._null()));
-				ifStatement._then().assign(delegateField, JExpr._new(delegateClass).arg(JExpr._this()));
-				invoke = delegateField.invoke(method.getName());
+				if(staticMethod) {
+					invoke = delegateClass.staticInvoke(method.getName());
+				} else {
+					final JConditional ifStatement = implMethod.body()._if(delegateField.eq(JExpr._null()));
+					ifStatement._then().assign(delegateField, JExpr._new(delegateClass).arg(JExpr._this()));
+					invoke = delegateField.invoke(method.getName());
+				}
 			}
 			for (final MethodParameterType param : method.getParam()) {
 				invoke.arg(param.getName());
 			}
 			implMethod.body()._return(invoke);
 		}
-	}
-
-	private int parseOptions(final String[] args, int i, final Map<String, Setter<String>> setters) throws BadCommandLineException {
-		for (final String name : setters.keySet()) {
-			if (args.length > i + 1) {
-				if (args[i + 1].equalsIgnoreCase(name)) {
-					if (args.length > i + 2 && !args[i + 2].startsWith("-")) {
-						setters.get(name).set(args[i + 2]);
-						i += 2;
-					} else {
-						throw new BadCommandLineException(MessageFormat.format(DelegatePlugin.RESOURCE_BUNDLE.getString("exception.missingArgument"), name));
-					}
-				} else if (args[i + 1].toLowerCase().startsWith(name + "=")) {
-					setters.get(name).set(args[i + 1].substring(name.length() + 1));
-					i++;
-				}
-			} else {
-				return 0;
-			}
-		}
-		return i;
 	}
 
 	private JType parseType(final JCodeModel model, final String typeSpec) {
@@ -253,9 +226,5 @@ public class DelegatePlugin extends Plugin {
 				return val;
 		}
 		return null;
-	}
-
-	private interface Setter<T> {
-		void set(final T val);
 	}
 }
